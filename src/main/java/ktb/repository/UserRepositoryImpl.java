1b9eeffad67a5b63455ac145decb51ad07cd2578
mdgome <mdgome3@gmail.com>
2025-10-13 22:44:36 +0900

    fix: UserRepository Bean 등록

--- src/main/java/ktb/repository/UserRepositoryImpl.java
+++ src/main/java/ktb/repository/UserRepositoryImpl.java
@@ -5,8 +5,10 @@ import ktb.db.user.UserData;
 import ktb.domain.UserAccount;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
 
 @Primary
+@Repository
 @RequiredArgsConstructor
 public class UserRepositoryImpl implements UserRepository {
     @Override
