a2532fc05b77d7be80acdd5ed382154bb42889ce
mdgome <mdgome3@gmail.com>
2025-10-15 01:27:12 +0900

    refactor: 게시글 수정 Request 검증 조건 및 Json 명 변경

--- src/main/java/ktb/dto/request/ArticlePatchRequest.java
+++ src/main/java/ktb/dto/request/ArticlePatchRequest.java
@@ -1,11 +1,14 @@
 package ktb.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 
 public record ArticlePatchRequest(
         @NotNull String title,
         @NotNull String content,
         String articleImagePath,
-        @NotNull Long userId //Session 도입 시 삭제
+        @NotNull
+        @JsonProperty("user_id")
+        Long userId //Session 도입 시 삭제
 ) {
 }
