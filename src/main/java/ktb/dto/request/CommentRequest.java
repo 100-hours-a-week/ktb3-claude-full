c5a1f446ad68d71f5f3cb594c3d5bea31b0a2f39
mdgome <mdgome3@gmail.com>
2025-10-15 01:00:16 +0900

    refactor: 댓글 Request 검증 조건, 메시지, Request 변수 명  수정

--- src/main/java/ktb/dto/request/CommentRequest.java
+++ src/main/java/ktb/dto/request/CommentRequest.java
@@ -1,12 +1,17 @@
 package ktb.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import ktb.constant.MessageConstant.CommentMessage;
 
 public record CommentRequest(
         @NotNull
+        @JsonProperty("user_id")
         Long userAccountId,
 
-        @NotNull
+        @NotBlank(message = CommentMessage.EMPTY_CONTENT)
+        @JsonProperty("comment_content")
         String content
 ) {
 }
