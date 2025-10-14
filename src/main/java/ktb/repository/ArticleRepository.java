c87d26ebf5a50668c7cd44b0d14aba17b567a69e
mdgome <mdgome3@gmail.com>
2025-10-14 21:15:29 +0900

    refactor: 사용하지 않는 메소드 및 기능 삭제

--- src/main/java/ktb/repository/ArticleRepository.java
+++ src/main/java/ktb/repository/ArticleRepository.java
@@ -5,7 +5,6 @@ import ktb.common.pagination.Slice;
 import ktb.domain.Article;
 import ktb.domain.ArticleComment;
 import ktb.domain.UserAccount;
-import ktb.dto.ArticleDto;
 
 public interface ArticleRepository {
     Optional<Article> findById(Long id);
@@ -18,14 +17,10 @@ public interface ArticleRepository {
 
     void like(Long id);
 
-    void save(ArticleDto dto);
     void save (Article article);
 
-    void update(Article originArticle, Article updateArticle);
 
     void deleteById(Long id);
-
-    void updateContent(Long id, String newTitle, String newContent);
 
     // ✅ 댓글
     ArticleComment addComment(Long articleId, String content, UserAccount user);
