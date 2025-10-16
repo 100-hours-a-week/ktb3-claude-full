90299ec8788e96f4c47bfed721be8e2258704ea6
mdgome <mdgome3@gmail.com>
2025-10-16 14:01:35 +0900

    docs: 게시글 API Swagger 설명 추가

--- src/main/java/ktb/controller/ArticleController.java
+++ src/main/java/ktb/controller/ArticleController.java
@@ -1,5 +1,11 @@
 package ktb.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 import ktb.common.pagination.Slice;
@@ -28,11 +34,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Article", description = "게시글 리소스 관련 API")
 @RestController
 @RequiredArgsConstructor
 public class ArticleController {
     private final ArticleService articleService;
 
+    @Operation(
+            summary = "Article search(page)",
+            description = "게시글들의 페이지 조회합니다.",
+            tags = { "Article" },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "성공", content = @Content(schema = @Schema(implementation = AllArticleRetrieveRequest.class))),
+            }
+    )
     @GetMapping("/articles")
     public ResponseEntity<ArticlesResponse<ArticleSimpleDto>> getAll(@Valid @RequestBody AllArticleRetrieveRequest request) {
         PageInfoDto pageInfo = PageInfoDto.of(request.after(), request.limit());
@@ -42,6 +57,14 @@ public class ArticleController {
         return ResponseEntity.ok(ArticlesResponse.of(Success.RETRIEVAL_ALL, page.getData(), page.getPageInfo()));
     }
 
+    @Operation(
+            summary = "Article insert",
+            description = "게시글 단건 등록합니다.",
+            tags = { "Article" },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "성공", content = @Content(schema = @Schema(implementation = ArticleRequest.class))),
+            }
+    )
     @PostMapping("/article")
     public ResponseEntity<Void> insertArticle(@Valid @RequestBody ArticleRequest request) {
         articleService.save(SaveArticleDto.of(request));
@@ -49,16 +72,34 @@ public class ArticleController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(
+            summary = "Article search(detail)",
+            description = "게시글 상세 조회합니다.",
+            tags = { "Article" },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "성공"),
+            }
+    )
     @GetMapping("/article/{id}")
-    public ResponseEntity<CommonResponse<ArticleDetailDto>> getOne(@PathVariable Long id) {
+    public ResponseEntity<CommonResponse<ArticleDetailDto>> getOne(
+            @Parameter(name = "id", description = "게시글 ID", required = true) @PathVariable Long id
+    ) {
         ArticleDetailDto response = articleService.findByIdDetail(id);
 
         return ResponseEntity.ok(CommonResponse.of(Success.RETRIEVAL_POST, response));
     }
 
+    @Operation(
+            summary = "Article update",
+            description = "게시글 수정합니다.",
+            tags = { "Article" },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "성공", content = @Content(schema = @Schema(implementation = ArticlePatchRequest.class))),
+            }
+    )
     @PatchMapping("/article/{id}")
     public ResponseEntity<Void> patchArticle(
-            @PathVariable Long id,
+            @Parameter(name = "id", description = "게시글 ID", required = true) @PathVariable Long id,
             @Valid @RequestBody ArticlePatchRequest request
     ) {
         articleService.save(SaveArticleDto.of(id, request));
@@ -66,8 +107,18 @@ public class ArticleController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(
+            summary = "Article Delete",
+            description = "게시글 삭제합니다.",
+            tags = { "Article" },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "성공"),
+            }
+    )
     @DeleteMapping("/article/{id}")
-    public ResponseEntity<Void> deleteArticle(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteArticle(
+            @Parameter(name = "id", description = "게시글 ID", required = true) @PathVariable Long id
+    ) {
         articleService.delete(id);
 
         return ResponseEntity.noContent().build();
--- src/main/java/ktb/dto/request/ArticleRequest.java
+++ src/main/java/ktb/dto/request/ArticleRequest.java
@@ -1,20 +1,26 @@
 package ktb.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+@Schema(description = "게시글 던건 등록 요청 구조")
 public record ArticleRequest(
+        @Schema(description = "게시글 제목", example = "게시글 제목")
         @NotNull
         @Size(max = 26)
         String title,
 
         @NotNull
+        @Schema(description = "게시글 내용", example = "게시글 내용")
         String content,
 
+        @Schema(description = "게시글에 첨부할 이미지 경로", example = "https://image.com/image.png")
         @Nullable
         String articleImagePath,
 
+        @Schema(description = "등록 시도한 유저 ID(Sequential ID)", example = "1")
         @NotNull
         Long userId // Session 도입 시 제거 예정
 ) {
