fe50066f76f6324f92d89e59a05ac371e0579b07
mdgome <mdgome3@gmail.com>
2025-11-16 23:28:19 +0900

    style: [BE] 유저 API 응답 값 필드명 snake_case 통일

--- src/main/java/ktb/dto/UserAccountDto.java
+++ src/main/java/ktb/dto/UserAccountDto.java
@@ -1,11 +1,24 @@
 package ktb.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import ktb.domain.UserAccount;
 
 public record UserAccountDto(
+        @Schema(description = "유저 ID", example = "1")
+        @JsonProperty("id")
         Long id,
+
+        @Schema(description = "유저 이메일", example = "example@example.com")
+        @JsonProperty("email")
         String email,
+
+        @Schema(description = "유저 닉네임", example = "닉네임예시")
+        @JsonProperty("nickname")
         String nickName,
+
+        @Schema(description = "유저 프로필 이미지", example = "http://image.com/1")
+        @JsonProperty("profile_image_path")
         String profileImagePath
 ) {
     public static UserAccountDto from(UserAccount userAccount) {
